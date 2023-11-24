1739a07349c191b279113e158901253194e98710
Paul Chesa <paulchesa1@gmail.com>
2023-11-25 00:03:52 +0300

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'job-listing',
+    loadComponent: () => import('./pages/job-listings/job-listings.component').then((m) => m.JobListingsComponent),
+  },
+  { path: '',   redirectTo: '/job-listing', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
