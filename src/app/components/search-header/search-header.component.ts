25214f8b83643d34591f51473142c40b75210df8
Paul Chesa <paulchesa1@gmail.com>
2023-11-25 00:05:26 +0300

    Added  Search header

---
+++ src/app/components/search-header/search-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-header.component.html',
+  styleUrl: './search-header.component.scss'
+})
+export class SearchHeaderComponent {
+
+}
