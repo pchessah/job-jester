490c7cfb61f046aa709284c7b9107f0c0aee08db
Paul Chesa <paulchesa1@gmail.com>
2023-11-25 13:21:16 +0300

    Add details about a card

--- src/app/components/job-listing-card/job-listing-card.component.ts
+++ src/app/components/job-listing-card/job-listing-card.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Job } from 'src/app/interfaces/job.interface';
 
 @Component({
   selector: 'app-job-listing-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './job-listing-card.component.html',
-  styleUrl: './job-listing-card.component.scss'
+  styleUrl: './job-listing-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class JobListingCardComponent {
+  @Input({required:true}) job!:Job
 
 }
+
