bffa31aeb8b8080915f1017f87127aab11213135
Paul Chesa <paulchesa1@gmail.com>
2023-11-25 13:20:54 +0300

    Implement search by text feature

--- src/app/components/search-header/search-header.component.ts
+++ src/app/components/search-header/search-header.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JobsService } from 'src/app/services/jobs.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-header.component.html',
   styleUrl: './search-header.component.scss'
 })
 export class SearchHeaderComponent {
+
+  searchString:string = "";
+  jobService: JobsService = inject(JobsService);
 
 }
