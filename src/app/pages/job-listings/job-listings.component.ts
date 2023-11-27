524474e78be68b6b098dc3a80def2c224be2e4f7
Paul Chesa <paulchesa1@gmail.com>
2023-11-27 08:36:18 +0300

    Update job listing page with paginator and empty list template

--- src/app/pages/job-listings/job-listings.component.ts
+++ src/app/pages/job-listings/job-listings.component.ts
@@ -4,11 +4,12 @@ import { SearchHeaderComponent } from 'src/app/components/search-header/search-h
 import { FilterBarComponent } from 'src/app/components/filter-bar/filter-bar.component';
 import { JobListingCardComponent } from 'src/app/components/job-listing-card/job-listing-card.component';
 import { JobsService } from 'src/app/services/jobs.service';
+import { JobsPaginatorComponent } from 'src/app/components/jobs-paginator/jobs-paginator.component';
 
 @Component({
   selector: 'app-job-listings',
   standalone: true,
-  imports: [CommonModule, SearchHeaderComponent, FilterBarComponent, JobListingCardComponent],
+  imports: [CommonModule, SearchHeaderComponent, FilterBarComponent, JobListingCardComponent, JobsPaginatorComponent],
   templateUrl: './job-listings.component.html',
   styleUrl: './job-listings.component.scss'
 })
