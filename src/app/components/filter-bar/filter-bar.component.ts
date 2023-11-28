c5e472d6086f006b13f4e5e6b8a57c8362131a8b
Paul Chesa <paulchesa1@gmail.com>
2023-11-28 07:33:56 +0300

    Place sort component, styling changes, pass correct input

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterSelectorComponent } from '../filter-selector/filter-selector.component';
 import { JobsService } from 'src/app/services/jobs.service';
+import { SorterComponent } from '../sorter/sorter.component';
 
 @Component({
   selector: 'app-filter-bar',
   standalone: true,
-  imports: [CommonModule, FilterSelectorComponent],
+  imports: [CommonModule, FilterSelectorComponent, SorterComponent],
   templateUrl: './filter-bar.component.html',
   styleUrl: './filter-bar.component.scss'
 })
